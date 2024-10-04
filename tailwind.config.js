533b689e9301c6a9233fe1bf8f23ccdac96bfc03
Raihan <raihanm854@gmail.com>
2024-10-04 21:49:30 +0700

    warna

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ["./src/**/*.{html,js,php}"],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        backdrop: "95%",
+      },
+      dropShadow: {
+        "3xl": "0 5px 3px rgba(0, 0, 2, 0.25)",
+        "4xl": ["0 5px 5px rgba(0, 0, 1, 0.5)"],
+      },
+    },
+    colors: {
+      nav: "#F2F0E2",
+      blues: "#00113D",
+      blues2: "#4BBCC7",
+      yellow: "#FFD700",
+      grey: "#EBEBEB",
+      black: "#000000",
+      white: "#FFFFFF",
+      red: "#FF0000",
+      green: "#00B01C",
+    },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#00113D",
+        },
+      },
+    ],
+  },
 };
