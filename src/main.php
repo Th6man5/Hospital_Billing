ed347a84f8a2b1848aac1e49e21767a180857b9f
rnnfl <renno.hangara@gmail.com>
2024-10-04 21:21:39 +0700

    a

--- src/main.php
+++ src/main.php
@@ -35,14 +35,14 @@
     <?php include 'template/sidebar.php'; ?>
     <div class="p-4 sm:ml-64">
         <div class="p-4 mt-14">
-            <h1>Dashboard</h1>
+            <h1>Dash</h1>
             <div class="grid grid-cols-4 gap-4 mb-4">
                 <div class="grid grid-rows-3 h-36 rounded bg-gray-50 dark:bg-gray-800 col-span-2 bg-blues2">
                     <p class="flex justify-center items-center text-2xl text-gray-400 dark:text-gray-500  card-title">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-nav" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                             <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                         </svg>
-                        New Users
+                        New User
                     </p>
                     <h1 class="flex justify-center items-center"></h1>
                     <div class="flex justify-end items-center">
