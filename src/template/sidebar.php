2ca547266e082c7ca075103ce813acc97aad74a7
Raihan <raihanm854@gmail.com>
2024-10-09 21:55:10 +0700

    main page updat

--- src/template/sidebar.php
+++ src/template/sidebar.php
@@ -14,8 +14,10 @@
      <div class="h-full px-3 pb-4 overflow-y-auto bg bg-sidenav dark:bg-gray-800">
          <ul class="space-y-2 font-medium">
              <li>
-                 <a href="http://localhost/hospital_billing/src/transaksi/transaksi.php" class="flex items-center p-2 text-gray-900 rounded-lg text-black hover:bg-black hover:text-white group">
-                 <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-nav" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z"/></svg>
+                 <a href="http://localhost/hospital_billing/src/main.php" class="flex items-center p-2 text-gray-900 rounded-lg text-black hover:bg-black hover:text-white group">
+                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-nav" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor">
+                         <path d="M520-600v-240h320v240H520ZM120-440v-400h320v400H120Zm400 320v-400h320v400H520Zm-400 0v-240h320v240H120Zm80-400h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z" />
+                     </svg>
                      <span class="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
                  </a>
              </li>
