a19f6e9a198186ec515ed6997821b14744ee341a
Raihan <raihanm854@gmail.com>
2024-10-10 15:53:42 +0700

    waran

--- src/dokter/dokter_delete.php
+++ src/dokter/dokter_delete.php
@@ -3,6 +3,7 @@ if (isset($_GET['id'])) {
     include('../database/database.php');
     $id = $_GET['id'];
     $sql = "DELETE FROM dokter WHERE id_dokter = $id";
+
     if (mysqli_query($conn, $sql)) {
         header('Location: dokter.php');
     } else {
